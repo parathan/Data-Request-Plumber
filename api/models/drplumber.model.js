d8fa38e4bcc9d8480111f2a3f49c29247b88c302
Parathan Vakeesan <parathan243@gmail.com>
2020-05-24 19:26:52 -0400

    Creating models

---
+++ api/models/drplumber.model.js
@@ -0,0 +1,33 @@
+var mongoose = require('mongoose');
+
+var requestSchema = new mongoose.Schema({
+  sender : {
+    type : String,
+    required : true
+  },
+  receiver : {
+    type : String,
+    required: true
+  },
+  createdOn : {
+    type : Date,
+    default : Date.now
+  },
+  deadline : {
+    type : Date,
+    required : true
+  },
+  specifications : {
+    type : String,
+    required : true
+  }
+});
+
+var userSchema = new mongoose.Schema({
+  name : {
+    type : String,
+    required : true
+  },
+  outgoing : [requestSchema],
+  incoming : [requestSchema]
+});
