b47dcf144800515cbaffc294c4cfc1e63db7191e
Parathan Vakeesan <parathan243@gmail.com>
2020-05-25 00:19:06 -0400

    Setting up routes

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,2 +1,32 @@
 var express = require('express');
 var router = express.Router();
+
+var ctrlUsers = require('../controllers/users.controllers.js');
+var ctrlRequests = require('../controllers/requests.controllers.js');
+
+// Hotel routes
+router
+  .route('/users')
+  .get(ctrlHotels.hotelsGetAll)
+  .post(ctrlHotels.hotelsAddOne);
+
+router
+  .route('/users/:userId')
+  .get(ctrlHotels.hotelsGetOne)
+  .put(ctrlHotels.hotelsUpdateOne)
+  .delete(ctrlHotels.hotelsDeleteOne);
+
+
+// Review routes
+router
+  .route('/users/:userId/requests')
+  .get(ctrlReviews.reviewsGetAll)
+  .post(ctrlReviews.reviewsAddOne);
+
+router
+  .route('/users/:userId/requests/:requestId')
+  .get(ctrlReviews.reviewsGetOne)
+  .put(ctrlReviews.reviewsUpdateOne)
+  .delete(ctrlReviews.reviewsDeleteOne);
+
+module.exports = router;
